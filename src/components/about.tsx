b244c17f7f76b9a2a10a8e6cee717bde88389319
humbertoham <hamduranhumberto@gmail.com>
2023-12-14 13:33:09 -0600

    first commit

---
+++ src/components/about.tsx
@@ -0,0 +1,25 @@
+
+export default function About() {
+    return (
+      <>
+        
+        <section className="bg-white " id="acerca">
+    <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div className="font-light text-gray-500 sm:text-lg ">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">¿Quiénes somos?</h2>
+            <p className="mb-4">Hub Inmobiliario y Estudio de Arquitectura es una empresa que se dedica a brindar servicios integrales en el ámbito inmobiliario y arquitectónico. Nuestro objetivo principal es ayudar a nuestros clientes a materializar sus proyectos de construcción, remodelación o compra/venta de propiedades, ofreciendo soluciones personalizadas y profesionales.</p>
+            <p className="mb-4">En el área inmobiliaria, contamos con un equipo de expertos en el mercado de bienes raíces, que se encargan de asesorar a nuestros clientes en la compra, venta o alquiler de propiedades. Realizamos un análisis exhaustivo de las necesidades y preferencias de cada cliente, para encontrar la mejor opción que se ajuste a sus requerimientos. Además, gestionamos todo el proceso de negociación y trámites legales, garantizando una transacción segura y transparente.</p>
+            <p className="mb-4">En cuanto al estudio de arquitectura, contamos con un equipo de arquitectos y diseñadores altamente capacitados, que se encargan de desarrollar proyectos arquitectónicos de calidad y funcionalidad. Realizamos diseños personalizados, teniendo en cuenta las necesidades y gustos de nuestros clientes, así como las normativas y regulaciones vigentes. Nos especializamos en la construcción de viviendas, edificios comerciales, oficinas, entre otros.</p>
+        </div>
+        <div className="grid grid-cols-2 gap-4 mt-8">
+            <img className="w-full rounded-lg" src="./images/about1.jpg" alt="office content 1"/>
+            <img className="mt-4 w-full lg:mt-10 rounded-lg" src="./images/about2.jpg" alt="office content 2"/>
+        </div>
+    </div>
+    
+</section>
+      s
+      </>
+    )
+  }
+  
